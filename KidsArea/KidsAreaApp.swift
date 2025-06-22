8285899bfbe38010e1063633f4d98ee75df8f99f
Omar Kader <ramooor700@gmail.com>
2025-06-22 03:52:34 +0300

    Initial Commit

---
+++ KidsArea/KidsAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsAreaApp.swift
+//  KidsArea
+//
+//  Created by OmAr Kader on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct KidsAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
